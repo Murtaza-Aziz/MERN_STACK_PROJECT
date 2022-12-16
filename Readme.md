e3096cfd3bcaf70dc171f3d0570bbe3ad2753107
MurtazaAzizMQ46717684 <murtaza.aziz@students.mq.edu.au>
2022-12-16 20:33:26 +1100

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+MERN project that is a part of the MERN_Fronted repository and is still a work in progress.
---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.MyNotesApp.com",
+  "https://MyNotesApp.com",
+];
+
+module.exports = allowedOrigins;
